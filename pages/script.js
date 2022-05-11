e0c02cf3640b9ed95300f42ec308cea2d5d68374
Izorgin Pavel <izorgin.pasha@yandex.ru>
2022-05-11 12:38:46 +0500

    feat: js

---
+++ pages/script.js
@@ -0,0 +1,27 @@
+let popap = document.querySelector('.popap');
+let editButton = document.querySelector('.profile-info__edit-button');
+let closeButton = document.querySelector('.popap__close');
+let formElement =document.querySelector('.popap');
+
+function openPopap(){
+    popap.classList.add('popap_opened');
+}
+function closePopap(){
+    popap.classList.remove('popap_opened');
+}
+function formSubmitHandler (evt) {
+    evt.preventDefault();
+    let nameInput = formElement.querySelector('.popap__fio');
+    let jobInput = formElement.querySelector('.popap__hobby');
+    let fioValue = nameInput.value;
+    let hobbyValue = jobInput.value;
+    let fio = document.querySelector('.profile-info__fio');
+    let hobby = document.querySelector('.profile-info__edit-button');
+    fio.textContent= fioValue;
+    hobby.textContent = hobbyValue;
+    closePopap();
+}
+
+editButton.addEventListener('click',openPopap);
+closeButton.addEventListener('click',closePopap);
+formElement.addEventListener('submit', formSubmitHandler); 
