1f0171b629751336f7e257c9294b7f913a6b7b60
Izorgin Pavel <izorgin.pasha@yandex.ru>
2022-05-14 12:54:56 +0500

    fix: js

--- scripts/script.js
+++ scripts/script.js
@@ -1,27 +1,26 @@
-let popap = document.querySelector('.popap');
 let editButton = document.querySelector('.profile__edit-button');
 let closeButton = document.querySelector('.popap__close');
 let formElement =document.querySelector('.popap');
+let fioValue = formElement.querySelector('.popap__fio');
+let hobbyValue = formElement.querySelector('.popap__hobby');
+let fio = document.querySelector('.profile__fio');
+let hobby = document.querySelector('.profile__hobby');
 
-function openPopap(){
-    popap.classList.add('popap_opened');
+function openPopap(){           //функция открытия всплывающего блока
+    formElement.classList.add('popap_opened');
+    fioValue.value=fio.textContent;
+    hobbyValue.value=hobby.textContent;
 }
-function closePopap(){
-    popap.classList.remove('popap_opened');
+function closePopap(){ // функция закрытия всплывающего елемента
+    formElement.classList.remove('popap_opened');
 }
-function formSubmitHandler (evt) {
+function formSubmitHandler (evt) { // функция обрабочик кнопки сохранить
     evt.preventDefault();
-    let nameInput = formElement.querySelector('.popap__fio');
-    let jobInput = formElement.querySelector('.popap__hobby');
-    let fioValue = nameInput.value;
-    let hobbyValue = jobInput.value;
-    let fio = document.querySelector('.profile__fio');
-    let hobby = document.querySelector('.profile__hobby');
-    fio.textContent= fioValue;
-    hobby.textContent = hobbyValue;
+    fio.textContent= fioValue.value;
+    hobby.textContent = hobbyValue.value;
     closePopap();
 }
 
-editButton.addEventListener('click',openPopap);
-closeButton.addEventListener('click',closePopap);
-formElement.addEventListener('submit', formSubmitHandler); 
+editButton.addEventListener('click',openPopap); // слушатель кнопки открытия окна редактирования профиля
+closeButton.addEventListener('click',closePopap); // слушатель кнопки закрытия окна редактирования
+formElement.querySelector('.popap__form').addEventListener('submit', formSubmitHandler);  // слушатель кнопки сохранить у окна редактирования профиля
