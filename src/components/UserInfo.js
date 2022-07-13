464b9e7f06399e51b1079891dea04d2aaf1a8010
Izorgin Pavel <izorgin.pasha@yandex.ru>
2022-07-13 11:15:14 +0500

    classUserInfo finish

---
+++ src/components/UserInfo.js
@@ -0,0 +1,13 @@
+class UserInfo{
+    constructor({selectorUser,selectorUserInfo}){
+        this._profileUser = document.querySelector(selectorUserInfo);
+        this._profileUserInfo = document.querySelector(selectorUserInfo);
+    }
+    getUserInfo(){
+        const UserInfo = { profileName: "Жак-Ив Кусто", profileInfo: "Исследователь океана" };
+    }
+    setUserInfo({profileName, profileInfo}){
+        this._profileUser.textContent = profileName;
+        this._profileUserInfo.textContent = profileInfo;
+    }
+}
