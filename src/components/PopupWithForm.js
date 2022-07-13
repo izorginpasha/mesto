d0527834ea924dd4388f6f4f6891fe93e735b3d1
Izorgin Pavel <izorgin.pasha@yandex.ru>
2022-07-13 10:25:06 +0500

    classForm finish

--- src/components/PopupWithForm.js
+++ src/components/PopupWithForm.js
@@ -1,10 +1,21 @@
 import { Popup } from "./Popup";
 
 export class PopupWithForm extends Popup{
-    constructor(){
-
+    constructor(popupSelector, sabmitForm){// принимает селектор класса popup
+        this._popupSelector= popupSelector;
+        this._sabmitForm = sabmitForm;
+        this._buttonForm = this._popap.querySelector('.popup__button');
+        this._formPopup = this._popap.querySelector('.popup__form');
+    }
+    _getInputValues(){//собирает данные полеи
+        return this._popap.querySelectorAll('.popup__text');
+    }
+    setEventListeners(){
+        super.setEventListeners();
+        this._buttonForm.addEventListener('submit', sabmitForm);
     }
     close(){
-        
+        this._formPopup.reset();
+        super.close();
     }
 }
